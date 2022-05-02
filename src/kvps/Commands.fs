26bfc4c6ae6d2616d0dd5ef9798375a1b2b505e9
tonycknight <tonycknight@users.noreply.github.com>
2022-05-02 19:03:41 +0100

    clean up imported tags

--- src/kvps/Commands.fs
+++ src/kvps/Commands.fs
@@ -223,8 +223,17 @@ module Commands=
                                                     Object.ReferenceEquals(data.data, null) then
                                                     invalidOp "The file was not a valid JSON file."
 
+                                                let cleanTags (kv: KeyValue) = 
+                                                    kv.tags 
+                                                    |> Option.nullToOption
+                                                    |> Option.map (fun xs -> xs 
+                                                                                |> Seq.filter (String.IsNullOrWhiteSpace >> not)
+                                                                                |> Array.ofSeq )
+                                                    |> Option.defaultValue [||]
+                                                let data = { data with data = data.data |> Array.map (fun kv -> { kv with tags = cleanTags kv } ) }
+
                                                 let validationErrors = 
-                                                    data.data 
+                                                    data.data                                                             
                                                             |> Seq.map (fun kv ->   if String.IsNullOrWhiteSpace(kv.key) ||
                                                                                         String.IsNullOrWhiteSpace(kv.value) then
                                                                                         Some "Invalid key/value data found."
@@ -238,8 +247,7 @@ module Commands=
                                                 let kvRepo = repo sp
 
                                                 let mutable count = 0
-                                                for kv in data.data do
-                                                    
+                                                for kv in data.data do                                                    
                                                     let! x = kvRepo.SetValueAsync(kv)
                                                     if x then
                                                         count <- count + 1
