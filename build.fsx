4b7016bfb692f89bc98f61b546322617e36006a4
tonycknight <tonycknight@users.noreply.github.com>
2023-02-25 19:24:35 +0000

    add fantomas to build scripts

--- build.fsx
+++ build.fsx
@@ -5,7 +5,7 @@ nuget Fake.DotNet.MSBuild
 nuget Fake.BuildServer.GitHubActions
 nuget Fake.Core.Target //"
 #if !FAKE
-  #load "./.fake/fakebuild.fsx/intellisense.fsx"
+#load "./.fake/fakebuild.fsx/intellisense.fsx"
 #endif
 
 open Fake.Core
@@ -20,109 +20,144 @@ let packageDir = "./package"
 let publishDir = "./publish"
 let mainSolution = "./kvps-cli.sln"
 
-let runNumber = (match Fake.BuildServer.GitHubActions.Environment.CI false with
-                    | true -> Fake.BuildServer.GitHubActions.Environment.RunNumber
-                    | _ -> "0")
+let runNumber =
+    (match Fake.BuildServer.GitHubActions.Environment.CI false with
+     | true -> Fake.BuildServer.GitHubActions.Environment.RunNumber
+     | _ -> "0")
+
 let commitSha = Fake.BuildServer.GitHubActions.Environment.Sha
-let versionSuffix = match Fake.BuildServer.GitHubActions.Environment.Ref with
-                    | null 
-                    | "refs/heads/main" ->  ""
-                    | _ ->                  "-preview"
 
-let version =  sprintf "0.1.%s%s" runNumber versionSuffix
-let infoVersion = match commitSha with
-                    | null -> version
-                    | sha -> sprintf "%s.%s" version sha                    
+let versionSuffix =
+    match Fake.BuildServer.GitHubActions.Environment.Ref with
+    | null
+    | "refs/heads/main" -> ""
+    | _ -> "-preview"
 
-let assemblyInfoParams (buildParams)=
-    [ ("Version", version); ("AssemblyInformationalVersion", infoVersion) ] |> List.append buildParams
+let version = sprintf "0.1.%s%s" runNumber versionSuffix
 
-let codeCoverageParams (buildParams)=
-    [   ("CollectCoverage", "true"); 
-        ("CoverletOutput", "./TestResults/coverage.info"); 
-        ("CoverletOutputFormat", "cobertura");        
-    ]  |> List.append buildParams
+let infoVersion =
+    match commitSha with
+    | null -> version
+    | sha -> sprintf "%s.%s" version sha
+
+let assemblyInfoParams (buildParams) =
+    [ ("Version", version); ("AssemblyInformationalVersion", infoVersion) ]
+    |> List.append buildParams
+
+let codeCoverageParams (buildParams) =
+    [ ("CollectCoverage", "true")
+      ("CoverletOutput", "./TestResults/coverage.info")
+      ("CoverletOutputFormat", "cobertura") ]
+    |> List.append buildParams
 
 let packBuildParams (buildParams) =
-    [ ("PackageVersion", version); ] |> List.append buildParams
+    [ ("PackageVersion", version) ] |> List.append buildParams
 
-let buildOptions = fun (opts: DotNet.BuildOptions) -> 
-                                { opts with
-                                    Configuration = DotNet.BuildConfiguration.Release;
-                                    MSBuildParams = { opts.MSBuildParams with Properties = assemblyInfoParams opts.MSBuildParams.Properties; WarnAsError = Some [ "*" ]; }
-                                    }
+let buildOptions =
+    fun (opts: DotNet.BuildOptions) ->
+        { opts with
+            Configuration = DotNet.BuildConfiguration.Release
+            MSBuildParams =
+                { opts.MSBuildParams with
+                    Properties = assemblyInfoParams opts.MSBuildParams.Properties
+                    WarnAsError = Some [ "*" ] } }
+
 let restoreOptions = fun (opts: DotNet.RestoreOptions) -> opts
 
-let testOptions (opts: DotNet.TestOptions)=
+let testOptions (opts: DotNet.TestOptions) =
     let properties = codeCoverageParams opts.MSBuildParams.Properties
 
-    { opts with NoBuild = false; 
-                Configuration = DotNet.BuildConfiguration.Debug;  // Temporary, to ensure Coverlet can find otherwise optimised-out code
-                Logger = Some "trx;LogFileName=test_results.trx";
-                Filter = Some "OS!=Windows";
-                MSBuildParams = { opts.MSBuildParams with Properties = properties } }
+    { opts with
+        NoBuild = false
+        Configuration = DotNet.BuildConfiguration.Debug // Temporary, to ensure Coverlet can find otherwise optimised-out code
+        Logger = Some "trx;LogFileName=test_results.trx"
+        Filter = Some "OS!=Windows"
+        MSBuildParams =
+            { opts.MSBuildParams with
+                Properties = properties } }
 
-let packOptions = fun (opts: DotNet.PackOptions) -> 
-                                { opts with 
-                                    Configuration = DotNet.BuildConfiguration.Release; 
-                                    NoBuild = false; 
-                                    MSBuildParams = { opts.MSBuildParams with Properties = (packBuildParams opts.MSBuildParams.Properties |> assemblyInfoParams )};
-                                    OutputPath = Some packageDir }
+let packOptions =
+    fun (opts: DotNet.PackOptions) ->
+        { opts with
+            Configuration = DotNet.BuildConfiguration.Release
+            NoBuild = false
+            MSBuildParams =
+                { opts.MSBuildParams with
+                    Properties = (packBuildParams opts.MSBuildParams.Properties |> assemblyInfoParams) }
+            OutputPath = Some packageDir }
 
 let publishProjects = !! "src/**/kvps.fsproj" |> List.ofSeq
 
 Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs
-    
+    !! "src/**/bin" ++ "src/**/obj" |> Shell.cleanDirs
+
     !! "tests/**/bin"
     ++ "tests/**/obj"
     ++ "tests/**/TestResults"
     ++ packageDir
     ++ publishDir
-    |> Shell.cleanDirs
-)
+    |> Shell.cleanDirs)
 
-Target.create "Restore" (fun _ ->
-    !! mainSolution
-    |> Seq.iter (DotNet.restore restoreOptions)
-)
+Target.create "Restore" (fun _ -> !!mainSolution |> Seq.iter (DotNet.restore restoreOptions))
 
-Target.create "Build" (fun _ ->
-    !! mainSolution
-    |> Seq.iter (DotNet.build buildOptions)
-)
+Target.create "Build" (fun _ -> !!mainSolution |> Seq.iter (DotNet.build buildOptions))
 
-Target.create "Pack" (fun _ -> publishProjects |> Seq.iter (DotNet.pack packOptions ) )
+Target.create "Pack" (fun _ -> publishProjects |> Seq.iter (DotNet.pack packOptions))
 
-Target.create "Unit Tests" (fun _ ->
-    !! "tests/**/*.fsproj"
-    |> Seq.iter (DotNet.test testOptions)    
-)
+Target.create "Unit Tests" (fun _ -> !! "tests/**/*.fsproj" |> Seq.iter (DotNet.test testOptions))
 
-Target.create "Consolidate code coverage" (fun _ ->  
-    let args = sprintf @"-reports:""./tests/**/coverage.info"" -targetdir:""./%s/codecoverage"" -reporttypes:""Html""" publishDir
+Target.create "Consolidate code coverage" (fun _ ->
+    let args =
+        sprintf
+            @"-reports:""./tests/**/coverage.info"" -targetdir:""./%s/codecoverage"" -reporttypes:""Html"""
+            publishDir
+
     let result = DotNet.exec id "reportgenerator" args
-  
-    if not result.OK then failwithf "reportgenerator failed!"  
-)
+
+    if not result.OK then
+        failwithf "reportgenerator failed!")
 
 Target.create "SCA" (fun _ ->
     let args = "package --vulnerable --include-transitive"
     let result = DotNet.exec id "list" args
-    if not result.OK then failwithf "dotnet sca failed!"  
-)
+
+    if not result.OK then
+        failwithf "dotnet sca failed!")
+
+Target.create "Check Format" (fun _ ->
+    let args = ". --recurse --check"
+    let result = DotNet.exec id "fantomas" args
+
+    if result.OK then
+        Trace.log "No files need formatting"
+    elif result.ExitCode = 99 then
+        failwith "Some files need formatting, run build with `Apply Style Rules` to resolve this."
+    else
+        failwithf "Errors while checking formatting: %A" result.Errors)
+
+Target.create "Apply Style Rules" (fun _ ->
+    let args = ". --recurse"
+    let result = DotNet.exec id "fantomas" args
+
+    if result.OK then
+        Trace.log "No files need formatting"
+    else
+        failwithf "Errors while applying formatting: %A" result.Errors)
 
 Target.create "All" ignore
 
 "Clean"
-  ==> "Restore"
-  ==> "Build"
-  ==> "Pack"
-  ==> "Unit Tests"
-  ==> "Consolidate code coverage"
-  ==> "SCA"
-  ==> "All"
+==> "Restore"
+==> "Check Format"
+==> "Build"
+==> "Unit Tests"
+==> "Consolidate code coverage"
+==> "Pack"
+
+"Restore" ==> "SCA"
+
+"Pack" ==> "All"
+
+"SCA" ==> "All"
 
 Target.runOrDefault "All"
